9a0159b953852cb16ec2a6b96823e416abcfe21b
scottcagno <scottiecagno@gmail.com>
2021-04-12 15:49:00 -0400

    sweet. just got a BASE server mux / router going. need to add some more stuff to it and do some http testing with it. anyway, cool beans.

--- pkg/web/middleware.go
+++ pkg/web/middleware.go
@@ -4,11 +4,6 @@ import (
 	"net/http"
 )
 
-type ctxEntry struct {
-	name string
-	data interface{}
-}
-
 func Render(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
 	http.Redirect(w, r, "/render?view="+name, http.StatusTemporaryRedirect)
 }
